5d6fceab413205294b7693e7817b84ec0b8575a1
ilyassstri <trikhaqiqi@gmail.com>
2021-04-06 10:34:43 +0700

    Memodifikasi

--- 02-Input-User/app.py
+++ 02-Input-User/app.py
@@ -2,6 +2,10 @@ name = input("Siapa nama kamu? ")
 # sentence = name + " Selamat Belajar Python" # contatination 
 hobby = input("Apa hobimu? ")
 
-cita_cita = input("Apa cita-cita kamu?")
+cita_cita = input("Apa cita-cita kamu? ")
+
+orang_berharga = input("Siapa orang yang paling berharga didalam hidupmu? ")
 
 print(name + " hobimu adalah " + hobby + " dan cita-cita kamu adalah menjadi " + cita_cita)
+print(orang_berharga + " adalah orang yang baling berharga dan kamu ingin membahagiakannya.")
+
