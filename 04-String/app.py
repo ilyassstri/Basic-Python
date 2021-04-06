2a988473804b457f4118510f003a1c3d358a9a07
ilyassstri <trikhaqiqi@gmail.com>
2021-04-06 10:43:48 +0700

    memodifikasi

--- 04-String/app.py
+++ 04-String/app.py
@@ -1,9 +1,14 @@
 # string adalah series of character susunan dari karakter karakter
 
 name = "M. Ilyas Tri Khaqiqi"
-name = 'Ilyassstri'
+# name = 'Ilyassstri'
+npm = "1194050"
+kelas = "D4-Teknik Informatika"
+almamater = "Politeknik Pos Indonesia"
 
-print(name[0:5])
-print(name[5:8])
-print(name[5:])
-print(name[:5])
+print(name[0:13], npm[0:3], kelas, almamater[11:15] )
+
+# print(name[0:5])
+# print(name[5:8])
+# print(name[5:])
+# print(name[:5])
