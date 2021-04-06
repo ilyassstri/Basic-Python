0276c937189c6a68a493d6240fdb7209874d63dc
ilyassstri <trikhaqiqi@gmail.com>
2021-04-06 10:54:33 +0700

    memodifikasi

--- 03-Type-Conversion/app.py
+++ 03-Type-Conversion/app.py
@@ -1,29 +1,35 @@
-# year = input("Tahun lahir :")
+year = input("Tahun lahir: ")
 
-# # print(type(year))
-# year = int(year)
-# # print(type(year))
+# print(type(year))
+year = int(year)
+# print(type(year))
 
-# age = 2021 - year
+age = 2021 - year
 
-# print("Umur kamu adalah " + str(age))
+print("Umur kamu adalah " + str(age))
 
-# kapan_lulus = input("Semester berapa kamu sekarang ?")
-# kapan_lulus = int(kapan_lulus)
+kapan_lulus = input("Semester berapa kamu sekarang? ")
+kapan_lulus = int(kapan_lulus)
 
-# lama_kuliah = 8 - kapan_lulus
+lama_kuliah = 8 - kapan_lulus
 
-# print("Kuliah kamu tinggal " + str(lama_kuliah) + " semester lagi")
-# coba_tanya = input("Siapa nama kamu? ")
-# umur = input("Berapa umurmu? ")
+print("Kuliah kamu tinggal " + str(lama_kuliah) + " semester lagi")
+coba_tanya = input("Siapa nama kamu? ")
+umur = input("Berapa umurmu? ")
 
-# print("nama saya adalah " + coba_tanya + umur)
+print("nama saya adalah " + coba_tanya + " " + umur + " tahun")
 
-# print(type(coba_tanya))
-# print(type(umur))
+print(type(coba_tanya))
+print(type(umur))
 
 umur = input("Berapa umur kamu? ")
 umur = int(umur)
+
+print("umur saya adalah " + str(umur) + " tahun")
+
+lama_hidup = umur
+
+print("lama hidup saya di dunia sudah " + str(lama_hidup) + " tahun")
 
 print(type(umur))
 
