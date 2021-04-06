2c61c8f614d08455228712a6a48350737923b116
ilyassstri <trikhaqiqi@gmail.com>
2021-04-06 10:37:59 +0700

    memodifikasi

--- 02-Input-User/app.py
+++ 02-Input-User/app.py
@@ -6,6 +6,8 @@ cita_cita = input("Apa cita-cita kamu? ")
 
 orang_berharga = input("Siapa orang yang paling berharga didalam hidupmu? ")
 
+harapan = input("Apa harapan kamu setelah lulus kuliah? ")
+
 print(name + " hobimu adalah " + hobby + " dan cita-cita kamu adalah menjadi " + cita_cita)
 print(orang_berharga + " adalah orang yang baling berharga dan kamu ingin membahagiakannya.")
-
+print("harapan saya setelah lulus kuliah " + harapan)
