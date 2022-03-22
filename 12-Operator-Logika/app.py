bd893ee4b86d187c8e35dadd6e3b60f10b2006e0
ilyassstri <trikhaqiqi@gmail.com>
2022-03-22 16:24:37 +0700

    modify

--- 12-Operator-Logika/app.py
+++ 12-Operator-Logika/app.py
@@ -1,42 +1,37 @@
 """ Contoh 1 """
 
-# Name = "M. Ilyas Tri Khaqiqi"
-# by_pass_validation = False
+Name = "M. Ilyas Tri Khaqiqi"
+by_pass_validation = False
 
-# if len(Name) > 3 or by_pass_validation : # memakai logika or
-#     print("welcome")
-# else:
-#     print("Nama terlalu pendek")
+if len(Name) > 3 or by_pass_validation : # memakai logika or
+    print("welcome")
+else:
+    print("Nama terlalu pendek")
 
-# if len(Name) > 3 and by_pass_validation : # memakai logika and
-#     print("welcome")
-# else:
-#     print("Nama terlalu pendek")
+if len(Name) > 3 and by_pass_validation : # memakai logika and
+    print("welcome")
+else:
+    print("Nama terlalu pendek")
 
 """ Contoh 2 """
 
-# movivasi = "Saya ingin menjadi seorang programmer dan dosen"
-# tantangan_berat = False
+motivasi = "Saya ingin menjadi seorang programmer dan dosen"
+tantangan_berat = False
 
-# if len(movivasi) > 3 or tantangan_berat:
-#     print("Aku akan menjadi orang sukses")
-# else:
-#     print("Saya pasti sukses!")
+if len(motivasi) > 3 or tantangan_berat:
+    print("Aku akan menjadi orang sukses")
+else:
+    print("Saya pasti sukses!")
 
 """ Contoh 3 """
 
-gajihan = "Genji hari rabu gajihan di kantornya"
-tanggal_muda = False
+gajihan = "Ilyas hari rabu gajihan di kantornya"
+tanggal_muda = True
 
 if len(gajihan) > 10 and tanggal_muda:
-    print("Genji akan membeli persediaan makan dan pakaian")
+    print("Ilyas akan membeli persediaan makan dan pakaian")
 else:
-    print("Genji masih belum punya uang")
-
-
-
-
-
+    print("Ilyas masih belum punya uang")
 
 """
     False or True = True
