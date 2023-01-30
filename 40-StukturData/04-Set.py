58227f5ca4e3903a9cfa97da68a41a43b9758d00
Ilyas Tri Khaqiqi <5493368-trikhaqiqi@users.noreply.gitlab.com>
2023-01-30 07:33:43 +0700

    added data structures

---
+++ 40-StukturData/04-Set.py
@@ -0,0 +1,17 @@
+# Creating a Set with
+# a mixed type of values
+# (Having numbers and strings)
+Set = set([1, 2, 'Kudang Koding', 4, 'For', 6, 'Kudang Koding'])
+print("\nSet with the use of Mixed Values")
+print(Set)
+
+# Accessing element using
+# for loop
+print("\nElements of set: ")
+for i in Set:
+	print(i, end =" ")
+print()
+
+# Checking the element
+# using in keyword
+print("Kudang Koding" in Set)
