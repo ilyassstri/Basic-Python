70a1e348697d9f71bed9e4df6554faac3b320cd1
trikhaqiqi <trikhaqiqi@gmail.com>
2023-09-25 20:04:23 +0700

    ok

--- 26-Fungsi/app.py
+++ 26-Fungsi/app.py
@@ -6,8 +6,12 @@ def halo_user(): # function dan nama function
     print("Halo user")
     print("Selamat belajar python")
 
+def preprocessing():
+    print("preprocessing")
+
 # parameter adalah nilai yang ditampung dan dilibatkan di dalam tanda kurung untuk dijalankan oleh fungsi
 
 print("Start")
 halo_user()
+preprocessing()
 print("Finish")
