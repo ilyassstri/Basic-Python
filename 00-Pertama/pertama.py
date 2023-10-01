2603728cfa58c757626ac20f61af74ca8893ede1
“trikhaqiqi” <trikhaqiqi@gmail.com>
2023-10-01 21:29:06 +0700

    ok

--- 00-Pertama/pertama.py
+++ 00-Pertama/pertama.py
@@ -7,7 +7,7 @@ print("Nama : M. Ilyas Tri Khaqiqi")
 print("NPM : 1194050") 
 print("D4-Teknik Informatika 2B " * 3) # sebuah ekspresi adalah sebuah intruks dan operasi yang menghasilkan hasil tertentu
 
-
+print("Saya sudah lulus")
 
 
 
