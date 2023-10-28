ac0c4a1f9fef5b55d5a41ab74708acc33953ee32
trikhaqiqi <trikhaqiqi@gmail.com>
2023-10-28 21:28:53 +0700

    ok

--- 00-Pertama/pertama.py
+++ 00-Pertama/pertama.py
@@ -9,6 +9,12 @@ print("D4-Teknik Informatika 2B " * 3) # sebuah ekspresi adalah sebuah intruks d
 
 print("Saya sudah lulus")
 
+""" 
+Kesimpulan dalam penulisan kodingan yaitu :
+1. memanfaatkan method atau function yang sudah ada di dalam file lain.
+2. memanfaatkan method atau function yang sudah dibuatkan di dalam bahasa atau librarynya
+3. tentang bagaimana kita menggunakan logika secara baik dalam penulisan kode program.
+"""
 
 
 
