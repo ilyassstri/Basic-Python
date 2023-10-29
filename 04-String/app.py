6aa792add606be4fda79d3288e57d636dae5c878
trikhaqiqi <trikhaqiqi@gmail.com>
2023-10-29 08:46:48 +0700

    memberi penjelasan bahasa python dalam pengunaan string

--- 04-String/app.py
+++ 04-String/app.py
@@ -10,6 +10,12 @@ ipk = "3.75"
 
 print(name[0:13], npm[0:3], kelas, almamater[11:15], npwp[8:12] )
 
+"""
+Kesimpulannya kalau di python ingin mengambil beberapa nilai di nilai string menggunakan format array []
+jadi [dari index ke berapa s/d ke berapa]
+
+"""
+
 # print(name[0:5])
 # print(name[5:8])
 # print(name[5:])
