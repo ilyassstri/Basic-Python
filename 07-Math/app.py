e909df0c75ebe18d11232e1520f1346136bef8ca
trikhaqiqi <trikhaqiqi@gmail.com>
2023-10-29 08:58:59 +0700

    penjelasan operasi matematika

--- 07-Math/app.py
+++ 07-Math/app.py
@@ -10,3 +10,18 @@ print(x // y) # Pembagian bulat
 print(x % y) # Modulus(Sisa pembagian)
 print(x * y) # Perkalian
 print(x ** y) # Perpangkatan
+
+a = 50
+b = 123123123123123
+c = 0
+print(a + b * c)
+
+"""
+Harus diingat kalau dalam operasi matematika ataupun di dalam kodingan.
+urutan yang dieksekusi terlebih dahulu adalah :
+1. kurung
+2. Pangkat
+3. Perkalian dan pembagian 
+4. Penjumlahan dan pengurungan
+
+"""
