5d40004375973f7d8b21e9d102bf4063b8e9fdaa
trikhaqiqi <trikhaqiqi@gmail.com>
2023-10-29 13:02:55 +0700

    module

--- 36-Module/app.py
+++ 36-Module/app.py
@@ -1,8 +1,16 @@
+# import matematika
+# from matematika import plus
 import matematika
-from matematika import plus
+from matematika import plus, divide, multiply
 
 result = matematika.plus(10, 8)
 print(result)
 
 result = plus(10, 16)
 print(result)
+
+divide = matematika.divide(5, 2)
+print(divide)
+
+multiply = matematika.multiply(100, 2)
+print(multiply)
--- 36-Module/matematika.py
+++ 36-Module/matematika.py
@@ -3,3 +3,9 @@ def plus(a, b):
 
 def minus(a, b):
     return a - b 
+
+def divide(a, b):
+    return a / b
+
+def multiply(a, b):
+    return a * b
