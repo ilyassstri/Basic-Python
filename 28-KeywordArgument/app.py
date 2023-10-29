0066e790e841f2f6e69e53d623692bd93279426f
trikhaqiqi <trikhaqiqi@gmail.com>
2023-10-29 11:53:05 +0700

    argumen

--- 28-KeywordArgument/app.py
+++ 28-KeywordArgument/app.py
@@ -1,6 +1,10 @@
 # Argumen adalah dimana akan tetap dijalankan sesuai dengan urutan fungsinya
 # klo tidak memakai argument manaya positional argument jadi urutannya itu harus benar
 
+"""
+Jadi kegunaan dari argumen ini untuk memberikan parameter yng tepat walaupun tidak sesuai dengan urutannya
+"""
+
 def halo_user(name, level): # function dan nama function
     print(f"Halo {name} - {level}")
     print("Selamat belajar python") 
@@ -13,3 +17,10 @@ print("Start")
 halo_user("ilyas", 10) # argumen
 # calculate_total_cost(total_price = 100000, shipping_cost=50000, discount=5000)
 print("Finish")
+
+print("Latihan membuat argumen")
+
+def hello_world(tingkat, kesulitan):
+    print(f"untuk posisimu akan memiliki {tingkat} dan {kesulitan}")
+
+hello_world(kesulitan="sangat sulit", tingkat="tinggi")
