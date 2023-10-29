81cc20b183dc6b2af824983b805aef7093f243cd
trikhaqiqi <trikhaqiqi@gmail.com>
2023-10-29 08:51:04 +0700

    ok

--- 05-Formated-String/app.py
+++ 05-Formated-String/app.py
@@ -3,8 +3,8 @@
 first_name = "M. Ilyas"
 last_name = "Tri Khaqiqi"
 hobby = "Lintening Music rock"
-
 kampus = "Universitas Logistik dan Bisnis Internasional"
+cita_cita = "Dosen"
 
 Message = first_name + " [" + last_name + "] "
 print(Message)
@@ -21,6 +21,6 @@ age = 31
 Message = f"Umur kamu {age}"
 print(Message)
 
-hasilnya = f"nama saya {first_name} {last_name} sama memiliki hobi {hobby}, dan saya sedang menempuh kuliah semester 8 di kampus {kampus}"
+hasilnya = f"nama saya {first_name} {last_name} sama memiliki hobi {hobby}, dan saya baru saja lulus di kampus {kampus}. Kemudian cita-cita saya adalah menjadi {cita_cita}"
 print(hasilnya)
 
